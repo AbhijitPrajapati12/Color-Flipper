51f58f97d6083352049fcf30e90a30346f9667ee
Abhijit Prajapati <sunnyprajapati3402@gmail.com>
2023-02-26 23:48:03 +0530

    Main

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "f15025"]
-const btn = document.getElementByID("btn");
+const colors = ["green", "yellow", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
-    //get random number betwenn 0-3
-    const randomNumber = 2;
-    document.body.stylr.backgroundColor = colors[randomNuber];
+    const randomNumber = getRandomNumber();
+    // console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
 
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
